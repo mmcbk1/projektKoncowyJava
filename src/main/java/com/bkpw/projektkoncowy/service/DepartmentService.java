cbe45e4edc000c4dfe363fec24e2274bbc134806
Wyrzel <Wyrzel86>
2018-06-30 14:20:22 +0200

    department

---
+++ src/main/java/com/bkpw/projektkoncowy/service/DepartmentService.java
@@ -0,0 +1,58 @@
+package com.bkpw.projektkoncowy.service;
+
+import com.bkpw.projektkoncowy.entity.Department;
+import com.bkpw.projektkoncowy.exception.NotFoundException;
+import com.bkpw.projektkoncowy.repository.CompanyRepository;
+import com.bkpw.projektkoncowy.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DepartmentService {
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+
+    public Department create(Department department){
+        return departmentRepository.save(department);
+    }
+
+    public List<Department> getAll(){
+        return departmentRepository.findAll();
+    }
+
+    public Department getOne(Long id){
+        Optional<Department> department =departmentRepository.findById(id);
+        if (!department.isPresent()) {
+            throw new NotFoundException(String.format("Department with id %s not found", id));
+        }
+
+        return department.get();
+    }
+
+    public void delete(Long id){
+        if (!departmentRepository.existsById(id)) {
+            throw new NotFoundException(String.format("Department with id %s not found", id));
+        }
+        departmentRepository.deleteById(id);
+    }
+
+    public Department update(Department department,Long id){
+        Optional<Department> OptionalDepartmentToUpdate =departmentRepository.findById(id);
+        if (!OptionalDepartmentToUpdate.isPresent()) {
+            throw new NotFoundException(String.format("Department with id %s not found", id));
+        }
+        Department departmentToUpdate=OptionalDepartmentToUpdate.get();
+
+        departmentToUpdate.setAddresses(department.getAddresses());
+        departmentToUpdate.setName(department.getName());
+        departmentToUpdate.setShortName(department.getShortName());
+
+        return departmentRepository.save(departmentToUpdate);
+    }
+
+
+}
