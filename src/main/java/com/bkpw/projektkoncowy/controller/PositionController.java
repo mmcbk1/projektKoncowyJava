66ffea3fe045df28bb17dc798d335163cf21e7d6
Wyrzel <Wyrzel86>
2018-07-07 12:16:34 +0200

    positions

--- src/main/java/com/bkpw/projektkoncowy/controller/PositionController.java
+++ src/main/java/com/bkpw/projektkoncowy/controller/PositionController.java
@@ -1,7 +1,12 @@
 package com.bkpw.projektkoncowy.controller;
 
+import com.bkpw.projektkoncowy.dto.PositionDTO;
+import com.bkpw.projektkoncowy.dto.UserDTO;
 import com.bkpw.projektkoncowy.entity.Position;
+import com.bkpw.projektkoncowy.entity.User;
+import com.bkpw.projektkoncowy.service.DepartmentService;
 import com.bkpw.projektkoncowy.service.PositionService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +20,17 @@ public class PositionController {
     @Autowired
     PositionService positionService;
 
+    @Autowired
+    DepartmentService departmentService;
+
+    @Autowired
+    ModelMapper modelMapper;
+
     @PostMapping("/position")
     @ResponseStatus(HttpStatus.CREATED)
-    public Position create(@RequestBody @Valid Position position) {
+    public Position create(@RequestBody @Valid PositionDTO positionDTO) {
+        Position position= convertToEntity(positionDTO);
+        position.setDepartment(departmentService.getOne(positionDTO.getDepartmentId()));
         return positionService.create(position);
     }
 
@@ -45,4 +58,15 @@ public class PositionController {
                           @RequestBody @Valid Position position) {
         return positionService.update(position, id);
     }
+
+    private Position convertToEntity(PositionDTO positionDTO)  {
+        Position position = modelMapper.map(positionDTO, Position.class);
+        return position;
+    }
+
+    private PositionDTO  entityToDTO(Position position){
+        PositionDTO positionDTO=modelMapper.map(position,PositionDTO.class);
+        return positionDTO;
+    }
+
 }
--- src/main/java/com/bkpw/projektkoncowy/dto/PositionDTO.java
+++ src/main/java/com/bkpw/projektkoncowy/dto/PositionDTO.java
@@ -1,4 +1,14 @@
 package com.bkpw.projektkoncowy.dto;
 
-public class PositionDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PositionDTO extends BaseEntityDTO {
+
+    private String name;
+    private Long departmentId;
 }
